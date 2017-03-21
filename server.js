293b3c7521058c39d5f03802db7dfedb2b68d2a0
Tiago Moreira <thmroot@gmail.com>
2017-03-21 16:45:31 +0000

    Final commit

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require("express");
 var app = express();
 
+
+function getLanguage(lng){
+    var str = lng.split(",");
+    return str[0].toString();
+}
+
+function getSoftware(hdrs){
+    var str = hdrs.split(") ");
+    var str2 = str[0].split(" (");
+    return str2[1].toString();
+}
+
 app.get('/', function(req,res){
-    res.send("HELLO WORLD");
+    var obj = {"ipaddress":req.headers['x-forwarded-for'].toString(),"language":getLanguage(req.headers['accept-language']),"software":getSoftware(req.headers['user-agent'])};
+    
+    console.log( obj );
+    res.send( obj );
 });
 
 app.listen(8080,function(){
